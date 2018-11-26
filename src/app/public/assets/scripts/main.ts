77a22cc985365dd195ec1f0c87bb52cb2f3d3fee
Fábio <fsmaiorano@gmail.com>
2018-11-26 11:50:43 -0200

    add: simple-mde with types and adjust render

--- src/app/public/assets/scripts/main.ts
+++ src/app/public/assets/scripts/main.ts
@@ -1,44 +1,29 @@
-// $(document).ready(function() {
-//   /**
-//    * Alerts
-//    */
-//   setTimeout(function() {
-//     var alert = document.querySelector(".alert");
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("document ready");
 
-//     if (alert) alert.className += " alert-hidden";
-//   }, 3000);
+  $(".mde").each(function() {
+    new SimpleMDE({
+      element: this,
+      spellChecker: false,
+      status: false
+    });
 
-//   /**
-//    * MDE Editor
-//    */
-//   $(".mde").each(function() {
-//     var simplemde = new SimpleMDE({
-//       element: this,
-//       spellChecker: false,
-//       status: false
-//     });
-//     // simplemde.render();
-//   });
+    $("[data-modal-open]").click(function(event) {
+      event.preventDefault();
 
-//   /**
-//    * Modals
-//    */
-$("[data-modal-open]").click(function(event) {
-  event.preventDefault();
+      $($(this).data("modal-open")).addClass("active");
+    });
 
-  $($(this).data("modal-open")).addClass("active");
+    $("[data-modal-close]").click(function(event) {
+      event.preventDefault();
+
+      $($(this).data("modal-close")).removeClass("active");
+    });
+
+    $(document).keydown(function(e) {
+      if (e.keyCode == 27) {
+        $(".modal").removeClass("active");
+      }
+    });
+  });
 });
-
-$("[data-modal-close]").click(function(event) {
-  event.preventDefault();
-
-  $($(this).data("modal-close")).removeClass("active");
-});
-
-$(document).keydown(function(e) {
-  if (e.keyCode == 27) {
-    $(".modal").removeClass("active");
-  }
-});
-
-console.log("teste");
